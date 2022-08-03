0890fee03a0c6f0c8688a2a10247f40bfaae3805
jaehee <jaehee23@naver.com>
2022-08-03 09:01:29 +0900

    리스트 컴포넌트 생성 (분리과정)

---
+++ src/components/List.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function List({todoData, setTodoData}) {
+
+    const btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "5px 9px",
+        borderRadius: "50%",
+        cursor: "pointer",
+        float: "right"
+    }
+
+    const getStyle = (completed) => {
+        return {
+            padding: "10px",
+            borderBottom: "1px #ccc dotted",
+            textDecoration: completed? "line-through" : "none"
+        }
+    }
+
+    const handleChangeComplete = (id) => {
+        let newTodoData = todoData.map(data=> {
+            if(data.id === id){
+                data.completed = !data.completed
+            }
+            return data;
+        })
+        setTodoData(newTodoData);
+    }
+
+    const handleClick = (id) => {
+        let newTodoData = todoData.filter((data) => data.id !== id);
+        setTodoData(newTodoData);
+    }
+
+    return (
+        <div>
+            {todoData.map((data)=> (
+                <div style={getStyle(data.completed)} key={data.id}>
+                    <input type="checkbox" defaultChecked={data.completed} onChange={()=>handleChangeComplete(data.id)}/> &nbsp;
+                    {data.title}
+                    <button style={btnStyle} onClick={()=> handleClick(data.id)}>x</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default List;
